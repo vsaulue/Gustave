b07f45d577b71ad50a5bc987b7f3e87a28c4d9ef
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2023-09-15 18:45:07 +0200

    SyncWorld: new high level component with scene & solver.
    Extremely barebone for now.

---
+++ components/core/include/gustave/world/SyncWorld.hpp
@@ -0,0 +1,167 @@
+/* This file is part of Gustave, a structural integrity library for video games.
+ *
+ * Copyright (c) 2022,2023 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files(the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <cassert>
+#include <optional>
+#include <utility>
+
+#include <gustave/cfg/cLibConfig.hpp>
+#include <gustave/cfg/cUnitOf.hpp>
+#include <gustave/cfg/LibTraits.hpp>
+#include <gustave/scenes/cuboidGrid/Scene.hpp>
+#include <gustave/solvers/force1/Solver.hpp>
+#include <gustave/utils/PointerHash.hpp>
+#include <gustave/world/WorldStructureState.hpp>
+
+namespace Gustave::World {
+    template<Cfg::cLibConfig auto cfg>
+    class SyncWorld {
+    private:
+        static constexpr auto u = Cfg::units(cfg);
+
+        template<Cfg::cUnitOf<cfg> auto unit>
+        using Real = Cfg::Real<cfg, unit>;
+
+        template<Cfg::cUnitOf<cfg> auto unit>
+        using Vector3 = Cfg::Vector3<cfg, unit>;
+    public:
+        using Scene = Scenes::CuboidGrid::Scene<cfg>;
+        using Solver = Solvers::Force1::Solver<cfg>;
+        class WorldStructure;
+
+        using BlockIndex = typename Scene::BlockIndex;
+        using SceneStructure = typename Scene::SceneStructure;
+        using SceneTransactionResult = typename Scene::TransactionResult;
+        using Solution = typename Solver::Solution;
+        using SolverStructure = typename Solver::Structure;
+        using SolverProblem = typename Solver::Problem;
+        using Structures = Utils::PointerHash::Map<SceneStructure const*, std::shared_ptr<WorldStructure>>;
+        using Transaction = typename Scene::Transaction;
+
+        class WorldStructure {
+        public:
+            using State = WorldStructureState;
+
+            [[nodiscard]]
+            explicit WorldStructure(std::shared_ptr<SceneStructure const> sceneStructure)
+                : sceneStructure_{ std::move(sceneStructure) }
+                , state_{ WorldStructureState::New }
+            {
+                assert(sceneStructure_);
+            }
+
+            [[nodiscard]]
+            bool contains(BlockIndex const& blockIndex) const {
+                return sceneStructure_->contains(blockIndex);
+            }
+
+            [[nodiscard]]
+            std::optional<Vector3<u.force>> forceVector(BlockIndex const& to, BlockIndex const& from) const {
+                assert(state_ == State::Solved);
+                auto const toIndex = sceneStructure_->solverIndexOf(to);
+                auto const fromIndex = sceneStructure_->solverIndexOf(from);
+                if (toIndex && fromIndex) {
+                    return solution_->forceVector(*toIndex, *fromIndex);
+                } else {
+                    return {};
+                }
+            }
+
+            void invalidate() {
+                state_ = State::Invalid;
+            }
+
+            [[nodiscard]]
+            SceneStructure const& sceneStructure() const {
+                return *sceneStructure_.get();
+            }
+
+            [[nodiscard]]
+            std::shared_ptr<SceneStructure const> const& sceneStructurePtr() const {
+                return sceneStructure_;
+            }
+
+            [[nodiscard]]
+            std::shared_ptr<SolverStructure const> solverStructurePtr() const {
+                return sceneStructure_->solverStructurePtr();
+            }
+
+            void solve(std::shared_ptr<Solution const> solution) {
+                assert(state_ == State::New);
+                assert(solution);
+                solution_ = std::move(solution);
+                state_ = State::Solved;
+            }
+
+            [[nodiscard]]
+            State state() const {
+                return state_;
+            }
+        private:
+            std::shared_ptr<SceneStructure const> sceneStructure_;
+            std::shared_ptr<Solution const> solution_;
+            State state_;
+        };
+
+        [[nodiscard]]
+        explicit SyncWorld(Vector3<u.length> const& blockSize, Vector3<u.acceleration> const& g, Solver solver)
+            : g_{ g }
+            , scene_{ blockSize }
+            , solver_{ std::move(solver) }
+        {}
+
+        [[nodiscard]]
+        std::shared_ptr<WorldStructure const> anyStructureContaining(BlockIndex const& blockIndex) const {
+            SceneStructure const* sceneStructure = scene_.anyStructureContaining(blockIndex);
+            if (sceneStructure != nullptr) {
+                return structures_.at(sceneStructure);
+            } else {
+                return { nullptr };
+            }
+        }
+
+        void modify(Transaction const& transaction) {
+            SceneTransactionResult const trResult = scene_.modify(transaction);
+            for (SceneStructure const* structure : trResult.deletedStructures()) {
+                auto const count = structures_.erase(structure);
+                assert(count == 1);
+            }
+            for (std::shared_ptr<SceneStructure const> const& sceneStructure : trResult.newStructures()) {
+                std::shared_ptr<WorldStructure> worldStructure = std::make_shared<WorldStructure>(sceneStructure);
+                auto const result = solver_.run(SolverProblem{ g_, worldStructure->solverStructurePtr() });
+                worldStructure->solve(result.solutionPtr());
+                auto const insertResult = structures_.insert({ &worldStructure->sceneStructure(), std::move(worldStructure)});
+                assert(insertResult.second);
+            }
+        }
+    private:
+        Vector3<u.acceleration> g_;
+        Scene scene_;
+        Solver solver_;
+        Structures structures_;
+    };
+}
---
+++ distribs/vanilla/tests/world/SyncWorld.cpp
@@ -0,0 +1,69 @@
+/* This file is part of Gustave, a structural integrity library for video games.
+ *
+ * Copyright (c) 2022,2023 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files(the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdexcept>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <gustave/world/SyncWorld.hpp>
+
+#include <TestConfig.hpp>
+
+using SyncWorld = Gustave::World::SyncWorld<G::libConfig>;
+using Solver = SyncWorld::Solver;
+
+static constexpr auto blockSize = vector3(1.f, 1.f, 1.f, u.length);
+static constexpr G::Real<u.density> concreteDensity = 2'400.f * u.density;
+static constexpr G::Real<u.mass> blockMass = blockSize.x() * blockSize.y() * blockSize.z() * concreteDensity;
+static constexpr float solverPrecision = 0.001f;
+
+[[nodiscard]]
+static SyncWorld makeWorld() {
+    auto const g = vector3(0.f, -10.f, 0.f, u.acceleration);
+    auto solver = Solver{ std::make_shared<Solver::Config>(1000, solverPrecision) };
+    return SyncWorld{ blockSize, g, std::move(solver) };
+}
+
+TEST_CASE("SyncWorld") {
+    SyncWorld world = makeWorld();
+    SyncWorld::Transaction transaction;
+    for (int i = 0; i < 10; ++i) {
+        transaction.addBlock({ {0,i,0}, concrete_20m, blockMass, i == 0 });
+    }
+    world.modify(transaction);
+
+    auto const structure = world.anyStructureContaining({ 0,0,0 });
+    REQUIRE(structure);
+    {
+        auto const force = structure->forceVector({ 0,0,0 }, { 0,1,0 });
+        REQUIRE(force);
+        CHECK_THAT(*force, M::WithinRel(9.f * blockMass * g, solverPrecision));
+    }
+    {
+        auto const force = structure->forceVector({ 0,0,0 }, { 0,2,0 });
+        REQUIRE(force);
+        CHECK(*force == vector3(0.f, 0.f, 0.f, u.force));
+    }
+}
