28a4694afde2bba0e7b0e4a2de24b9fcc0f2bcfd
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2024-06-20 15:30:36 +0200

    IndexRange: new utils class.

---
+++ components/utils/include/gustave/utils/IndexRange.hpp
@@ -0,0 +1,97 @@
+/* This file is part of Gustave, a structural integrity library for video games.
+ *
+ * Copyright (c) 2022-2024 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files(the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <cassert>
+#include <concepts>
+#include <limits>
+#include <type_traits>
+#include <span>
+#include <vector>
+
+namespace gustave::utils {
+    template<typename T>
+    concept cRangeIndex = std::unsigned_integral<T> && !std::is_const_v<T>;
+
+    template<cRangeIndex Index_>
+    class IndexRange {
+    public:
+        using Index = Index_;
+
+        [[nodiscard]]
+        constexpr IndexRange()
+            : start_{ 0 }
+            , size_{ 0 }
+        {}
+
+        [[nodiscard]]
+        constexpr IndexRange(Index start, Index size)
+            : start_{ start }
+            , size_{ size }
+        {
+            assert(size_ >= 0);
+            assert(std::numeric_limits<Index>::max() - size_ >= start_);
+        }
+
+        [[nodiscard]]
+        constexpr Index start() const {
+            return start_;
+        }
+
+        [[nodiscard]]
+        constexpr Index end() const {
+            return start_ + size_;
+        }
+
+        [[nodiscard]]
+        constexpr Index size() const {
+            return size_;
+        }
+
+        template<typename T>
+        [[nodiscard]]
+        constexpr std::span<T const> subSpanOf(std::vector<T> const& vec) const {
+            assert(start_ + size_ <= vec.size());
+            return { vec.begin() + start_, size_ };
+        }
+
+        template<typename T>
+        [[nodiscard]]
+        constexpr std::span<T> subSpanOf(std::span<T> span) const {
+            assert(start_ + size_ <= span.size());
+            return { span.data() + start_, size_ };
+        }
+
+        [[nodiscard]]
+        constexpr bool operator==(IndexRange const&) const = default;
+    private:
+        Index start_;
+        Index size_;
+    };
+
+    template<cRangeIndex Index>
+    IndexRange(Index, Index) -> IndexRange<Index>;
+}
---
+++ components/utils/tests/utils/IndexRange.cpp
@@ -0,0 +1,63 @@
+/* This file is part of Gustave, a structural integrity library for video games.
+ *
+ * Copyright (c) 2022-2024 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files(the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <cstdint>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <gustave/utils/IndexRange.hpp>
+
+namespace utils = gustave::utils;
+
+TEST_CASE("utils::IndexRange") {
+    auto ir = utils::IndexRange<std::uint32_t>{ 2,3 };
+
+    SECTION(".end()") {
+        CHECK(ir.end() == 5);
+    }
+
+    SECTION(".size()") {
+        CHECK(ir.size() == 3);
+    }
+
+    SECTION(".start()") {
+        CHECK(ir.start() == 2);
+    }
+
+    SECTION(".subSpanOf(std::span)") {
+        auto const vec = std::vector<int>{ -1, -2, -3, -4, -5, -6 };
+        auto const span = std::span<int const>{ vec };
+        auto const res = ir.subSpanOf(span);
+        CHECK(res.data() == &span[2]);
+        CHECK(res.size() == 3);
+    }
+
+    SECTION(".subSpanOf(std::vector)") {
+        auto const vec = std::vector<int>{ -1,-2,-3,-4,-5,-6 };
+        std::span<int const> const res = ir.subSpanOf(vec);
+        CHECK(res.data() == &vec[2]);
+        CHECK(res.size() == 3);
+    }
+}
