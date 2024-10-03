bc12f1f260afbf4e4eb53726accdad73419912ec
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2024-10-03 22:28:35 +0200

    conanfile: add ignored exports_sources.

--- conanfile.py
+++ conanfile.py
@@ -40,7 +40,7 @@ class GustaveRecipe(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
 
     exports = "LICENSE.txt"
-    exports_sources = "cmake/*", "components/*", "distribs/*", "packaging/*", "tools/*", "CMakeLists.txt"
+    exports_sources = "cmake/*", "components/*", "distribs/*", "packaging/*", "tools/*", "CMakeLists.txt", "!*/build", "!*/__pycache__"
     test_package_folder = "packaging/conan/test_package"
 
     def set_version(self):
