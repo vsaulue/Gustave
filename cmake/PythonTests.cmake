65f6de99c62be5cc37e3017f1b6927f340617606
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2024-10-03 22:28:05 +0200

    cmake: refactor args of python tests.

--- cmake/PythonTests.cmake
+++ cmake/PythonTests.cmake
@@ -1,19 +1,19 @@
 # This file is part of Gustave, a structural integrity library for video games.
-# 
+#
 # Copyright (c) 2022-2024 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
-# 
+#
 # MIT License
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files(the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions :
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
@@ -22,33 +22,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-add_executable(svgViewer
-    "main.cpp"
-)
-target_link_libraries(svgViewer
-    PRIVATE CLI11::CLI11
-    PRIVATE Distrib-Std-StrictUnit
-    PRIVATE Examples-JsonWorld
-)
+include_guard(GLOBAL)
 
-add_custom_target(run-svgViewer-test
-    COMMAND Python::Interpreter "${python_scripts_dir}/testSvgViewer.py"
-        "$<TARGET_FILE:svgViewer>"
-        "--samples-dir" "${json_samples_dir}"
-        ${python_test_args}
-    DEPENDS svgViewer
-)
-add_dependencies(run-tool-tests run-svgViewer-test)
+set(python_test_args)
 
-declare_memcheck_test(
-    TEST_ID "svgViewer-01"
-    TARGET svgViewer
-    ARGS "${json_samples_dir}/worlds/tower10.json"
-)
-declare_memcheck_test(
-    TEST_ID "svgViewer-02"
-    TARGET svgViewer
-    ARGS
-        "-r" "${json_samples_dir}/renderers/blockMaxStress.json"
-        "${json_samples_dir}/worlds/tower10.json"
-)
+if(CMAKE_COLOR_DIAGNOSTICS)
+    list(APPEND python_test_args "--colour-mode" "ansi")
+endif()
