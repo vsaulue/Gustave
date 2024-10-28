b2c9d6ed06249616febfa067927cc4877488868f
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2024-10-28 23:24:27 +0100

    testConanPackaging: rewrite into the new framework.

--- cmake/python/testConanPackaging.py
+++ cmake/python/testConanPackaging.py
@@ -25,58 +25,53 @@
 import argparse
 import json
 import re
-import subprocess
-import sys
-import tempfile
+import typing
 
 import gustaveUtils as gu
 
-def _extractGustaveRef(jsonObject: json) -> str:
-    nodes = jsonObject['graph']['nodes']
-    regex = re.compile(r'^gustave/')
-    result = None
-    for key,value in nodes.items():
-        tmpResult = value['ref']
-        res = regex.match(tmpResult)
-        if res != None:
-            result = tmpResult
-            break
-    if result == None:
-        raise ValueError('gustave dependency not found in the JSON object.')
-    return result
+class TestConanPackaging(gu.TestScript):
+    """Tests that the conanfile.py packaging works."""
+
+    @staticmethod
+    def _extractGustaveRef(jsonObject: dict[str,typing.Any]) -> str:
+        nodes = jsonObject['graph']['nodes']
+        regex = re.compile(r'^gustave/')
+        result = None
+        for key,value in nodes.items():
+            tmpResult = value['ref']
+            res = regex.match(tmpResult)
+            if res != None:
+                result = tmpResult
+                break
+        if result == None:
+            raise ValueError('gustave dependency not found in the JSON object.')
+        return result
+
+    # @typing.override
+    def makeArgsParser(self) -> argparse.ArgumentParser:
+        result = argparse.ArgumentParser(description='Checks that the packaging using the local conanfile works.')
+        return result
+
+    # @typing.override
+    def doRun(self, ctx: gu.TestScriptContext) -> None:
+        conanExe = ctx.cmakeVars.executables.conan
+
+        createCmd = [ conanExe, 'create', ctx.cmakeVars.folders.source,
+            f'-pr:b={ctx.cmakeVars.conanProfiles.build}',
+            f'-pr:h={ctx.cmakeVars.conanProfiles.host}',
+            '-s:h', 'build_type={0}'.format(ctx.cmakeVars.config),
+            '-c', 'tools.build:skip_test=True',
+            '--build=missing',
+            '--format=json'
+        ]
+        with ctx.newCmd(createCmd, separateStderr=True) as createRun:
+            with open(createRun.outPath) as stdoutFile:
+                createJson = json.load(stdoutFile)
+
+        # Cleanup
+        gustavePackageRef = self._extractGustaveRef(createJson)
+        cleanupCmd = [ conanExe, 'remove', gustavePackageRef, '--confirm' ]
+        ctx.runCmd(cleanupCmd, exitOnError=False)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='testConanPackaging', description='Checks that conan packaging works.')
-    parser.add_argument('sourceDir', help='Project base directory (containing conanfile.py)')
-    parser.add_argument('--colour-mode', dest='colorMode', choices=['ansi','none','auto'], default='auto', help='Color mode used in the standard output')
-    parser.add_argument('--build-type', dest='buildType', choices=['Debug','Release','RelWithDebInfo'], required=True, help='Build type')
-    parser.add_argument('--conan', required=True, type=gu.ExecutableParser(), help='Conan executable')
-    parser.add_argument('--pr-build', dest='prBuild', required=True, help='Conan build profile')
-    parser.add_argument('--pr-host', dest='prHost', required=True, help='Conan host profile')
-    args = parser.parse_args()
-    coloring = gu.Tty.makeColoring(args.colorMode)
-
-    cmd = [ args.conan, 'create', args.sourceDir,
-        '-pr:b={0}'.format(args.prBuild),
-        '-pr:h={0}'.format(args.prHost),
-        '-s:h', 'build_type={0}'.format(args.buildType),
-        '-c', 'tools.build:skip_test=True',
-        '--build=missing',
-        '--format=json'
-    ]
-    failedText = coloring('Test FAILED', 'red')
-    with tempfile.TemporaryFile(mode='w+') as stdoutFile:
-        with tempfile.TemporaryFile(mode='w+') as stderrFile:
-            conanCreate = subprocess.run(cmd, stdin=subprocess.DEVNULL, stdout=stdoutFile, stderr=stderrFile)
-            if conanCreate.returncode != 0:
-                stderrFile.seek(0)
-                print(stderrFile.read(), file=sys.stderr)
-                print('{0}: {1}'.format(failedText, ' '.join(cmd)), file=sys.stderr)
-                sys.exit(1)
-        stdoutFile.seek(0)
-        createJson = json.load(stdoutFile)
-
-    # Cleanup
-    gustavePackageRef = _extractGustaveRef(createJson)
-    cleanupCmd = [ args.conan, 'remove', gustavePackageRef, '--confirm' ]
-    subprocess.run(cleanupCmd, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    TestConanPackaging().run()
