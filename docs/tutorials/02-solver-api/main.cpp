3c84e91ae76a426095329099f6d9e6f62b64444f
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2025-07-31 18:00:50 +0200

    docs: use snippets for tuto-02

--- docs/tutorials/02-solver-api/main.cpp
+++ docs/tutorials/02-solver-api/main.cpp
@@ -26,27 +26,43 @@
 #include <iostream>
 #include <memory>
 
+// -8<- [start:distrib-unitless]
 // Choosing the Std Unitless distribution, with double precision
 #include <gustave/distribs/std/unitless/Gustave.hpp>
 
 using G = gustave::distribs::std::unitless::Gustave<double>;
+// -8<- [end:distrib-unitless]
 
+// -8<- [start:type-aliases]
 using Structure = G::Solvers::Structure;
 using Solver = G::Solvers::F1Solver;
+// -8<- [end:type-aliases]
+
+// -8<- [start:newSolver]
+[[nodiscard]]
+static Solver newSolver() {
+    auto const g = G::vector3(0.f, -10.f, 0.f); // gravity acceleration (metre/second²).
+    auto const solverPrecision = 0.01; // precision of the force balancer (here 1%).
+    return Solver{ Solver::Config{ g, solverPrecision } };
+}
+// -8<- [end:newSolver]
 
 int main() {
     std::cout << "\n\n--------------------\n";
     std::cout << "Step 1: New empty solver structure\n";
 
+    // -8<- [start:new-structure]
     auto structure = std::make_shared<Structure>();
     std::cout << "Structure of " << structure->nodes().size() << " blocks\n";
     std::cout << "Structure of " << structure->links().size() << " links\n";
+    // -8<- [end:new-structure]
 
 
 
     std::cout << "\n\n--------------------\n";
     std::cout << "Step 2: Add nodes (= blocks)\n";
 
+    // -8<- [start:add-blocks]
     auto const blockMass = 3'000.0; // kilogram
     //          xy
     auto const n00 = structure->addNode(Structure::Node{ blockMass, true });
@@ -58,12 +74,14 @@ int main() {
     auto const n20 = structure->addNode(Structure::Node{ blockMass, true });
     std::cout << "Structure of " << structure->nodes().size() << " blocks\n";
     std::cout << "Structure of " << structure->links().size() << " links\n";
+    // -8<- [end:add-blocks]
 
 
 
     std::cout << "\n\n--------------------\n";
     std::cout << "Step 3: Add links\n";
 
+    // -8<- [start:add-links]
     // { compression, shear, tensile } in Newton/metre
     auto const wallConductivity = G::Model::ConductivityStress{ 1'000'000.0, 500'000.0, 200'000.0 };
     auto const roofConductivity = G::Model::ConductivityStress{ 100'000.0, 500'000.0, 100'000.0 };
@@ -83,33 +101,38 @@ int main() {
 
     std::cout << "Structure of " << structure->nodes().size() << " blocks\n";
     std::cout << "Structure of " << structure->links().size() << " links\n";
+    // -8<- [end:add-links]
 
 
 
     std::cout << "\n\n--------------------\n";
     std::cout << "Step 4: Configure a solver\n";
 
-    auto const g = G::vector3(0.f, -10.f, 0.f); // gravity acceleration (metre/second²).
-    auto const solverPrecision = 0.01; // precision of the force balancer (here 1%).
-    auto const solver = Solver{ Solver::Config{ g, solverPrecision } };
+    // -8<- [start:configure-solver]
+    auto const solver = newSolver();
 
     std::cout << "Solver gravity vector = " << solver.config().g() << '\n';
     std::cout << "Solver target max error = " << solver.config().targetMaxError() << '\n';
+    // -8<- [end:configure-solver]
 
 
 
     std::cout << "\n\n--------------------\n";
     std::cout << "Step 5: Solve a structure\n";
 
+    // -8<- [start:solve-structure]
     auto const solverResult = solver.run(structure);
     std::cout << "solution.isSolved() = " << solverResult.isSolved() << '\n';
+    // -8<- [end:solve-structure]
 
 
 
     std::cout << "\n\n--------------------\n";
     std::cout << "Step 6: Inspect a solution's forces\n";
 
+    // -8<- [start:inspect-solution]
     auto const& solution = solverResult.solution();
     std::cout << "Force vector on block 00 by 01 = " << solution.contacts().at({l00_01, true}).forceVector() << '\n';
     std::cout << "Force vector on block 21 by 22 = " << solution.contacts().at({l20_21, false}).forceVector() << '\n';
+    // -8<- [end:inspect-solution]
 }
