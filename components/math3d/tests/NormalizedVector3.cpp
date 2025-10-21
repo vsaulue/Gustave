e8de1be8c236a233c7665b871fea6eeb001f716d
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2025-10-21 23:16:55 +0200

    NormalizedVector3: handle normalization of smaller vectors

--- components/math3d/tests/NormalizedVector3.cpp
+++ components/math3d/tests/NormalizedVector3.cpp
@@ -31,19 +31,34 @@
 #include <gustave/math3d/Vector3.hpp>
 #include <TestHelpers.hpp>
 
-using NormalizedVector3 = gustave::math3d::NormalizedVector3<rt, double>;
+using NormalizedVector3 = gustave::math3d::NormalizedVector3<rt, float>;
 
 namespace matchers = gustave::testing::matchers;
 
 TEST_CASE("NormalizedVector3") {
+    constexpr auto minNormalized = std::numeric_limits<float>::min();
+    auto const lowNormVec = vector3(0.5f * std::sqrt(minNormalized), 0.f, 0.f, u.one);
+
     SECTION("::NormalizedVector3(Real<one>, Real<one>, Real<one>)") {
-        NormalizedVector3 const v{ 2.f, -1.f, -2.f };
-        CHECK_THAT(v.value(), matchers::WithinRel(vector3(2.f, -1.f, -2.f, u.one) / 3.f, epsilon));
+        SECTION("// valid") {
+            auto const v = NormalizedVector3{ 2.f, -1.f, -2.f };
+            CHECK_THAT(v.value(), matchers::WithinRel(vector3(2.f, -1.f, -2.f, u.one) / 3.f, epsilon));
+        }
+
+        SECTION("// invalid (norm too low)") {
+            CHECK_THROWS_AS(NormalizedVector3( lowNormVec.x(), lowNormVec.y(), lowNormVec.z()), std::domain_error);
+        }
     }
 
     SECTION("::NormalizedVector3(Vector3<*> const&)") {
-        NormalizedVector3 const v{ vector3(4.f, -2.f, 4.f, u.mass) };
-        CHECK_THAT(v.value(), matchers::WithinRel(vector3(2.f, -1.f, 2.f, u.one) / 3.f, epsilon));
+        SECTION("// valid") {
+            auto const v = NormalizedVector3{ vector3(4.f, -2.f, 4.f, u.mass) };
+            CHECK_THAT(v.value(), matchers::WithinRel(vector3(2.f, -1.f, 2.f, u.one) / 3.f, epsilon));
+        }
+
+        SECTION("// invalid (norm too low)") {
+            CHECK_THROWS_AS(NormalizedVector3( lowNormVec ), std::domain_error);
+        }
     }
 
     SECTION("") {
