15e3d762cf9c02712f4c5b3d5c05c27765f6fee0
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2025-11-17 21:30:53 +0100

    Scene.BlockRef.Contacts: new class

---
+++ components/core/include/gustave/core/scenes/cuboidGridScene/blockReference/Contacts.hpp
@@ -0,0 +1,182 @@
+/* This file is part of Gustave, a structural integrity library for video games.
+ *
+ * Copyright (c) 2022-2025 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files(the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <cassert>
+
+#include <gustave/cfg/cLibConfig.hpp>
+#include <gustave/cfg/LibTraits.hpp>
+#include <gustave/core/scenes/common/cSceneUserData.hpp>
+#include <gustave/core/scenes/cuboidGridScene/detail/DataNeighbours.hpp>
+#include <gustave/core/scenes/cuboidGridScene/detail/SceneData.hpp>
+#include <gustave/core/scenes/cuboidGridScene/ContactReference.hpp>
+#include <gustave/core/scenes/cuboidGridScene/forwardDecls.hpp>
+#include <gustave/math3d/BasicDirection.hpp>
+#include <gustave/utils/Prop.hpp>
+
+namespace gustave::core::scenes::cuboidGridScene::blockReference {
+    namespace detail {
+        template<cfg::cLibConfig auto libCfg_, common::cSceneUserData UD_, bool isMut_>
+        class Enumerator {
+        private:
+            template<typename T>
+            using Prop = utils::Prop<isMut_, T>;
+
+            using Direction = math3d::BasicDirection;
+
+            using SceneData = cuboidGridScene::detail::SceneData<libCfg_, UD_>;
+        public:
+            using Value = cuboidGridScene::ContactReference<libCfg_, UD_, isMut_>;
+
+            [[nodiscard]]
+            Enumerator()
+                : scene_{ nullptr }
+                , localBlockId_{ utils::NO_INIT }
+                , direction_{ 6 }
+            {
+            }
+
+            [[nodiscard]]
+            explicit Enumerator(Prop<SceneData>& scene, BlockIndex const& localBlockId)
+                : scene_{ &scene }
+                , localBlockId_{ localBlockId }
+                , direction_{ 0 }
+            {
+                next();
+            }
+
+            [[nodiscard]]
+            bool isEnd() const {
+                return direction_ >= 6;
+            }
+
+            void operator++() {
+                ++direction_;
+                next();
+            }
+
+            [[nodiscard]]
+            Value operator*() const {
+                assert(not isEnd());
+                return value();
+            }
+
+            [[nodiscard]]
+            bool operator==(Enumerator const& other) const = default;
+        private:
+            void next() {
+                while (!isEnd() && !value().isValid()) {
+                    ++direction_;
+                }
+            }
+
+            [[nodiscard]]
+            Value value() const {
+                return Value{ *scene_, { localBlockId_, static_cast<Direction::Id>(direction_) } };
+            }
+
+            Prop<SceneData>* scene_;
+            BlockIndex localBlockId_;
+            int direction_;
+        };
+    }
+
+    template<cfg::cLibConfig auto libCfg_, common::cSceneUserData UD_, bool isMut_>
+    class Contacts {
+    public:
+        template<bool mut>
+        using ContactReference = cuboidGridScene::ContactReference<libCfg_, UD_, mut>;
+
+        using Direction = ContactReference<false>::ContactIndex::Direction;
+    private:
+        template<typename T>
+        using Prop = utils::Prop<isMut_, T>;
+
+        template<typename T>
+        using PropPtr = utils::PropPtr<isMut_, T>;
+
+        using SceneData = cuboidGridScene::detail::SceneData<libCfg_, UD_>;
+
+        template<bool mut>
+        using Enumerator = detail::Enumerator<libCfg_, UD_, mut>;
+    public:
+        using ConstIterator = utils::ForwardIterator<Enumerator<false>>;
+        using Iterator = utils::ForwardIterator<Enumerator<isMut_>>;
+
+        [[nodiscard]]
+        explicit Contacts(Prop<SceneData>& sceneData, BlockIndex const& blockId)
+            : scene_{ &sceneData }
+            , index_{ blockId }
+        {}
+
+        [[nodiscard]]
+        ContactReference<true> along(Direction direction)
+            requires (isMut_)
+        {
+            return doAlong(*this, direction);
+        }
+
+        [[nodiscard]]
+        ContactReference<false> along(Direction direction) const {
+            return doAlong(*this, direction);
+        }
+
+        [[nodiscard]]
+        Iterator begin()
+            requires (isMut_)
+        {
+            return Iterator{ *scene_, index_ };
+        }
+
+        [[nodiscard]]
+        ConstIterator begin() const {
+            return ConstIterator{ *scene_, index_ };
+        }
+
+        [[nodiscard]]
+        utils::EndIterator end() const {
+            return {};
+        }
+    private:
+        [[nodiscard]]
+        ContactReference<false> alongUnchecked(Direction direction) const {
+            return ContactReference<false>{ *scene_, { index_, direction } };
+        }
+
+        [[nodiscard]]
+        static auto doAlong(meta::cCvRefOf<Contacts> auto&& self, Direction direction) {
+            using Result = decltype(self.along(direction));
+            auto result = Result{ *self.scene_, { self.index_, direction } };
+            if (!result.isValid()) {
+                throw result.invalidError();
+            }
+            return result;
+        }
+
+        PropPtr<SceneData> scene_;
+        BlockIndex index_;
+    };
+}
---
+++ components/core/testing/scenes/cuboidGridScene/blockReference/Contacts.cpp
@@ -0,0 +1,138 @@
+/* This file is part of Gustave, a structural integrity library for video games.
+ *
+ * Copyright (c) 2022-2025 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files(the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions :
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <array>
+#include <ranges>
+
+#include <gustave/core/scenes/cuboidGridScene/blockReference/Contacts.hpp>
+#include <gustave/core/scenes/cuboidGridScene/detail/SceneData.hpp>
+#include <gustave/core/scenes/cuboidGridScene/detail/SceneUpdater.hpp>
+#include <gustave/testing/ConstDetector.hpp>
+
+#include <TestHelpers.hpp>
+#include <SceneUserData.hpp>
+
+namespace cuboid = gustave::core::scenes::cuboidGridScene;
+
+using SceneData = cuboid::detail::SceneData<libCfg, SceneUserData>;
+using SceneUpdater = cuboid::detail::SceneUpdater<libCfg, SceneUserData>;
+
+template<bool mut>
+using Contacts = cuboid::blockReference::Contacts<libCfg, SceneUserData, mut>;
+
+using BlockIndex = SceneData::Blocks::BlockIndex;
+using ContactIndex = Contacts<false>::ContactReference<false>::ContactIndex;
+using Direction = Contacts<false>::Direction;
+using Transaction = SceneUpdater::Transaction;
+
+static_assert(std::ranges::forward_range<Contacts<true>>);
+static_assert(std::ranges::forward_range<Contacts<false>>);
+
+TEST_CASE("core::scenes::cuboidGridScene::blockReference::Contacts") {
+    auto const blockSize = vector3(1.f, 1.f, 1.f, u.length);
+    SceneData scene{ blockSize };
+
+    Transaction t;
+    auto newBlock = [&](BlockIndex const& index, bool isFoundation) -> void {
+        t.addBlock({ index, concrete_20m, 1000.f * u.mass, isFoundation });
+    };
+    newBlock({ 1,1,1 }, false);
+    newBlock({ 0,1,1 }, true);
+    newBlock({ 2,1,1 }, false);
+    newBlock({ 1,0,1 }, true);
+    newBlock({ 1,2,1 }, false);
+    newBlock({ 1,1,0 }, false);
+    newBlock({ 1,1,2 }, false);
+    newBlock({ 0,0,9 }, false);
+    SceneUpdater{ scene }.runTransaction(t);
+
+    auto mContacts111 = Contacts<true>{ scene, {1,1,1} };
+    auto const& cmContacts111 = mContacts111;
+    auto iContacts111 = Contacts<false>{ scene, {1,1,1} };
+
+    auto mContacts009 = Contacts<true>{ scene, {0,0,9} };
+
+    auto contactId = [](BlockIndex const& localBlockId, Direction direction) {
+        return ContactIndex{ localBlockId, direction };
+    };
+
+    SECTION(".along()") {
+        auto runValidTest = [&](auto&& contacts, bool expectedConst) {
+            auto result = contacts.along(Direction::minusY());
+            REQUIRE(result.isValid());
+            CHECK(expectedConst == result.structure().userData().isCalledAsConst());
+        };
+
+        SECTION("// mutable") {
+            runValidTest(mContacts111, false);
+        }
+
+        SECTION("// const") {
+            runValidTest(cmContacts111, true);
+        }
+
+        SECTION("// immutable") {
+            runValidTest(iContacts111, true);
+        }
+
+        SECTION("// invalid") {
+            CHECK_THROWS_AS(mContacts009.along(Direction::plusY()), std::out_of_range);
+        }
+    }
+
+    SECTION(".begin() // & .end()") {
+        SECTION("// 6 contacts") {
+            auto runTest = [&](auto&& contacts, bool expectedConst) {
+                auto expectedIds = std::array{
+                    contactId({1,1,1}, Direction::minusX()),
+                    contactId({1,1,1}, Direction::plusX()),
+                    contactId({1,1,1}, Direction::minusY()),
+                    contactId({1,1,1}, Direction::plusY()),
+                    contactId({1,1,1}, Direction::minusZ()),
+                    contactId({1,1,1}, Direction::plusZ()),
+                };
+                auto ids = contacts | std::views::transform([](auto&& contact) { return contact.index(); });
+                REQUIRE_THAT(ids, matchers::c2::UnorderedRangeEquals(expectedIds));
+                CHECK(expectedConst == (*contacts.begin()).structure().userData().isCalledAsConst());
+            };
+
+            SECTION("// mutable") {
+                runTest(mContacts111, false);
+            }
+
+            SECTION("// const") {
+                runTest(cmContacts111, true);
+            }
+
+            SECTION("// immutable") {
+                runTest(iContacts111, true);
+            }
+        }
+
+        SECTION("// empty") {
+            CHECK(mContacts009.begin() == mContacts009.end());
+        }
+    }
+}
